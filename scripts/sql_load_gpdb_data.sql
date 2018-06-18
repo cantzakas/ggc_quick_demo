169609d1bd83a9ae1e384d9efc57101bd9bcb293
Constantinos (Costas) Antzakas <cantzakas@pivotal.io>
2018-06-18 17:04:14 +0100

    Update sql_load_gpdb_data.sql

--- scripts/sql_load_gpdb_data.sql
+++ scripts/sql_load_gpdb_data.sql
@@ -1,4 +1,4 @@
 INSERT INTO basic (id, col1, col2, col3)
 SELECT id, 'TEXT_'::text || id::text, id::integer, id::float4
 FROM (
-	SELECT GENERATE_SERIES(10000, 19999) AS id) A;
+	SELECT GENERATE_SERIES(0, 9999) AS id) A;
